8e9d615a2913bd52c4c15a331c610027ef3ce170
joshuaramoscs <joshita1999@gmail.com>
2020-06-21 20:59:49 -0600

    Hard coded the leds in sequence and the buttons to one led to test cirquit.

--- gpio_game.py
+++ gpio_game.py
@@ -4,14 +4,87 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
+#Setup GPIO OUT
 GPIO.setup(17, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(18, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(27, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(22, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(23, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(24, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(25, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(5, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(6, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(12, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(13, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(19, GPIO.OUT, initial=GPIO.LOW)
+
+#Setup GPIO IN
+GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+#Speed
+speed = .05
 
 for i in range(5):
 	GPIO.output(17, GPIO.HIGH)
-	time.sleep(.1)
+	time.sleep(speed)
 	GPIO.output(17, GPIO.LOW)
-	time.sleep(.1)
+	time.sleep(speed)
+
+	GPIO.output(18, GPIO.HIGH)
+	time.sleep(speed)
+	GPIO.output(18, GPIO.LOW)
+	time.sleep(speed)
+
+	GPIO.output(27, GPIO.HIGH)
+	time.sleep(speed)
+	GPIO.output(27, GPIO.LOW)
+	time.sleep(speed)
+
+	GPIO.output(22, GPIO.HIGH)
+	time.sleep(speed)
+	GPIO.output(22, GPIO.LOW)
+	time.sleep(speed)
+
+	GPIO.output(23, GPIO.HIGH)
+	time.sleep(speed)
+	GPIO.output(23, GPIO.LOW)
+	time.sleep(speed)
+
+	GPIO.output(24, GPIO.HIGH)
+	time.sleep(speed)
+	GPIO.output(24, GPIO.LOW)
+	time.sleep(speed)
+
+	GPIO.output(25, GPIO.HIGH)
+	time.sleep(speed)
+	GPIO.output(25, GPIO.LOW)
+	time.sleep(speed)
+
+	GPIO.output(5, GPIO.HIGH)
+	time.sleep(speed)
+	GPIO.output(5, GPIO.LOW)
+	time.sleep(speed)
+
+	GPIO.output(6, GPIO.HIGH)
+	time.sleep(speed)
+	GPIO.output(6, GPIO.LOW)
+	time.sleep(speed)
+
+	GPIO.output(12, GPIO.HIGH)
+	time.sleep(speed)
+	GPIO.output(12, GPIO.LOW)
+	time.sleep(speed)
+
+	GPIO.output(13, GPIO.HIGH)
+	time.sleep(speed)
+	GPIO.output(13, GPIO.LOW)
+	time.sleep(speed)
+
+	GPIO.output(19, GPIO.HIGH)
+	time.sleep(speed)
+	GPIO.output(19, GPIO.LOW)
+	time.sleep(speed)
 
 led_on = False
 def switch(ev=None):
@@ -19,7 +92,9 @@ def switch(ev=None):
 	led_on = not led_on
 	GPIO.output(17, GPIO.HIGH if led_on else GPIO.LOW)
 
-GPIO.add_event_detect(18, GPIO.FALLING, callback=switch, bouncetime=300)
+GPIO.add_event_detect(16, GPIO.FALLING, callback=switch, bouncetime=300)
+GPIO.add_event_detect(26, GPIO.FALLING, callback=switch, bouncetime=300)
 
 while True:
 	time.sleep(1)
+
