364d17661a608476a3ee207bb402d22fe7ad35cc
joshuaramoscs <joshita1999@gmail.com>
2020-06-23 21:01:49 -0600

    Cleaned up code and made it modular.

--- gpio_game.py
+++ gpio_game.py
@@ -1,86 +1,90 @@
 import RPi.GPIO as GPIO
 import time
 
+# **** SETUP GPIO ****
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-# Setup GPIO OUT
-GPIO.setup(17, GPIO.OUT, initial=0) #Red LED
-GPIO.setup(18, GPIO.OUT, initial=0)
-GPIO.setup(27, GPIO.OUT, initial=0)
-GPIO.setup(22, GPIO.OUT, initial=0)
-GPIO.setup(23, GPIO.OUT, initial=0)
-GPIO.setup(24, GPIO.OUT, initial=0)
-GPIO.setup(25, GPIO.OUT, initial=0)
-GPIO.setup(5, GPIO.OUT, initial=0)
-GPIO.setup(6, GPIO.OUT, initial=0)
-GPIO.setup(12, GPIO.OUT, initial=0)
-GPIO.setup(13, GPIO.OUT, initial=0)
-GPIO.setup(19, GPIO.OUT, initial=0) #Blue LED
-
-# Setup GPIO IN
-GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-# Save LED numbers in an array
+# Save LEDs and BUTTONs pin nums in an array for easy manipulation
 leds = [17, 18, 27, 22, 23,24, 25, 5, 6, 12, 13, 19]
+buttons = [16, 26]
 
-# Flashes all LEDs on and off in "s" seconds
-def all_leds_flash(s):
-    all_led_on()
-    time.sleep(s)
-    all_led_off()
-    time.sleep(s)
+# Setup GPIO OUT (LEDs)
+for i in leds:
+        GPIO.setup(i, GPIO.OUT, initial=0)
+
+# Setup GPIO IN (BUTTONs)
+for i in buttons:
+    GPIO.setup(i, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+
+# **** SINGLE LED METHODS ****
+# Turns off given LED
+def led_off(num):
+    GPIO.output(num, 0)
+
+# Turns on given LED
+def led_on(num):
+    GPIO.output(num, 1)
+
+# Flashes given LED in given seconds and takes bool to toggle last sleep
+def led_flash(num, sec, sleep_on):
+    led_on(num)
+    time.sleep(sec)
+    led_off(num)
+    if(sleep_on):
+        time.sleep(sec)
+
+
+# **** ALL LEDS METHODS ****
+# Turns off all LEDs
+def all_led_off(ev=None):
+    for i in leds:
+        led_off(i)
 
 # Turns on all LEDs
 def all_led_on(ev=None):
-    GPIO.output(17, 1)
-    GPIO.output(18, 1)
-    GPIO.output(27, 1)
-    GPIO.output(22, 1)
-    GPIO.output(23, 1)
-    GPIO.output(24, 1)
-    GPIO.output(25, 1)
-    GPIO.output(5, 1)
-    GPIO.output(6, 1)
-    GPIO.output(12, 1)
-    GPIO.output(13, 1)
-    GPIO.output(19, 1)
+    for i in leds:
+        led_on(i)
 
-# Turns off all LEDs
-def all_led_off(ev=None):
-    GPIO.output(17, 0)
-    GPIO.output(18, 0)
-    GPIO.output(27, 0)
-    GPIO.output(22, 0)
-    GPIO.output(23, 0)
-    GPIO.output(24, 0)
-    GPIO.output(25, 0)
-    GPIO.output(5, 0)
-    GPIO.output(6, 0)
-    GPIO.output(12, 0)
-    GPIO.output(13, 0)
-    GPIO.output(19, 0)
+# Flashes all LEDs in given seconds and takes bool to toggle last sleep
+def all_leds_flash(sec, sleep_on):
+    all_led_on()
+    time.sleep(sec)
+    all_led_off()
+    if(sleep_on):
+        time.sleep(sec)
+    
 
+# Traverses and flashes the LEDs individualy forward
+def traverse_led_f(sec):
+    for i in leds[1:]:
+        GPIO.output(i, 1)
+        time.sleep(sec)
+        GPIO.output(i, 0)
+
+# Traverses and flashes the LEDs individualy backward
+def traverse_led_b(sec):
+    for i in reversed(leds[:-1]):
+        GPIO.output(i, 1)
+        time.sleep(sec)
+        GPIO.output(i, 0)
+
+
+# **** Main ****
 # Detect input event
 GPIO.add_event_detect(16, GPIO.FALLING, callback=all_led_on, bouncetime=300)
 GPIO.add_event_detect(26, GPIO.FALLING, callback=all_led_off, bouncetime=300)
 
-# Speed in seconds
+# Start game
 speed = .25
+hold = .5
 for i in range(4):
-    all_leds_flash(speed)
+    all_leds_flash(speed, True)
 
-for i in leds:
-    GPIO.output(i, 1)
-    time.sleep(speed)
-    GPIO.output(i, 0)
-    
-for i in reversed(leds):
-    GPIO.output(i, 1)
-    time.sleep(speed)
-    GPIO.output(i, 0)
-
-#while True:
+led_flash(leds[:1], hold, False)
+while True:
+    traverse_led_f(speed)
+    traverse_led_b(speed)
     #time.sleep(1)
 
